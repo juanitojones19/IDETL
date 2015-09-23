9409dc0c7ba5f32ca52a0bdf5caf462b42972347
juanitojones19 <hastarm@gmail.com>
2015-09-23 13:10:19 -0500

    Se termino la funcionalidad, se cambiaron metodos a staticos, se pueden mejorar detalles

--- IDETL/src/com/Traductores/idetl/Controllers/VariableControlador.java
+++ IDETL/src/com/Traductores/idetl/Controllers/VariableControlador.java
@@ -18,9 +18,10 @@ import javax.swing.JOptionPane;
  */
 public class VariableControlador 
 {
-    private final String regex = "[^\\d]\\w*";
+    private static final String regex = "[^\\d]\\w*";
     private static List<String> listaVariablres = new ArrayList();
     private static boolean bandera = true;
+    private static String mensaje = "";
     private JDialog ventana;
     
     public VariableControlador( JDialog ventanaDialogo, String nombreVariable, String tipoValor, String valor)
@@ -49,23 +50,56 @@ public class VariableControlador
         }
     }// fin del constructor
     
-    public boolean esNombreValido(String nombre)
+    public static boolean validar(String nombreVariable, String tipoValor)
+    {
+        DiccionarioTipoDatos.cargarPalabras("TipodeVariables.txt");
+        Diccionario.cargarPalabras("palabrasReservadas.txt");
+        boolean valido = true;
+        if(esNombreValido(nombreVariable))
+        {
+            if(!esPalabraReservada(nombreVariable))// si no es palabra reservada sigue evaluando 
+            {
+                if(tipoValorValido(tipoValor))// si el tipo de valor corresponde a los que existe en java
+                {
+                    //JOptionPane.showMessageDialog(ventanaDialogo, "nombre de variable correcta, no es palabra reservada y es tipo de valor valido");
+                    //listaVariablres.add(nombreVariable);
+                    //agregarVariable(nombreVariable, tipoValor, valor);
+                                       
+                }else{// si no corresponden manda un mensaje
+                    //JOptionPane.showMessageDialog(ventanaDialogo, "Tipo de valor no valiod");
+                    valido = false;
+                    mensaje = tipoValor + "Tipo de valor no valiod";
+                }// fin del if - else
+            }else{// de lo contrario no sigue y manda un mensaje
+                //JOptionPane.showMessageDialog(ventanaDialogo, Diccionario.obtenerListaPalabras().get(nombreVariable));
+                mensaje = Diccionario.obtenerListaPalabras().get(nombreVariable).toString();
+                valido = false;
+            }
+        }else{// si no es un nombre valido manda un mensae de error
+            //JOptionPane.showMessageDialog(ventanaDialogo, "Nombre de variable invalido");
+            mensaje = nombreVariable + "Nombre de variable invalido";
+            valido = false;
+        }
+        return valido;
+    }
+    
+    public static boolean esNombreValido(String nombre)
     {
         return nombre.matches(regex);
     }
     
-    public boolean esPalabraReservada(String nombre)
+    public static boolean esPalabraReservada(String nombre)
     {
         return Diccionario.buscarPalabra(nombre);
     }
     
-    public boolean tipoValorValido(String tipoValor)
+    public static boolean tipoValorValido(String tipoValor)
     {
         return DiccionarioTipoDatos.buscarPalabra(tipoValor);
     }
     
     // metodo que busca dentro de una lista si el elemento esta repedito
-    public boolean yaSeAgrego(String nombreVariable)
+    public static boolean yaSeAgrego(String nombreVariable)
     {
         //System.out.println("Entra: " + nombreVariable);
         boolean indicador = false;
@@ -81,23 +115,34 @@ public class VariableControlador
     }//fin del metodo yaSeAgrego
     
     //metodo que agregar el nombre de una variable a una lista
-    public void agregarVariable(String nombreVariable, String tipoValor, String valor)
+    public static boolean agregarVariable(String nombreVariable, String tipoValor, String valor)
     {
+        boolean seAgrego = true;
         //System.out.println(bandera);
         if(bandera)// si es la primera vex que se agrega una variable a la clase
         {
             listaVariablres.add(nombreVariable);
             bandera = false;
             ArchivoXML.crearTagVarialbe(nombreVariable, tipoValor, valor);
+            mensaje = "Ya se agrego la variable: " + nombreVariable;
         }else{// de lo contrario
             if(yaSeAgrego(nombreVariable))// si el nombre de la variable esta repetido
             {
-                JOptionPane.showMessageDialog(ventana, "Ya se agrego la variable " + nombreVariable);//manda mensaje
+                //JOptionPane.showMessageDialog(ventana, "Ya se agrego la variable " + nombreVariable);//manda mensaje
+                mensaje = "Ya se agrego la variable: " + nombreVariable;
+                seAgrego = false;
             }else{// de lo contrario agrega el nombre de la variable
                 listaVariablres.add(nombreVariable);
                 ArchivoXML.crearTagVarialbe(nombreVariable, tipoValor, valor);
+                mensaje = "Se agrego correctamemente: " + nombreVariable;
             }//fin del if - else dentro del else
         }// fin del if - else
+        return seAgrego;
     }//fin del metodo agregarVariable
+    
+    public static String obtenerMensaje()
+    {
+        return mensaje;
+    }
     
 }//fin de la clase VariableControlador
